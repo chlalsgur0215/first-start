d36d88a30f6fab94b23f38f1b4c5e4512fde9d84
miyuck <chlalsgur0215@gmail.com>
2026-04-11 16:37:58 +0900

    Update HelloWorld.cpp

--- C++/Baekjoon/HelloWorld.cpp
+++ C++/Baekjoon/HelloWorld.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
-print("Hello World");
+using namespace std;
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
+//using namespace로 std생략 가능 > std::cout , std::endl
