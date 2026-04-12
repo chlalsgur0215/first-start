54a753bc63b67ce5fdeb15bd9a7e01648f6d3764
miyuck <chlalsgur0215@gmail.com>
2026-04-12 21:54:22 +0900

    Create 11021.cpp

---
+++ C++/Baekjoon/Level_3/11021.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int num, A, B;
+    cin >> num;
+    for (int i = 0; i < num; i++){
+        cin >> A >> B;
+        cout << "Case #" << i+1 << ": " << A + B << '\n';
+    }
+}
