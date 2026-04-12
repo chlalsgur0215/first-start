cb2560cac6ce6bee516bc31a3726765d4478a6da
miyuck <chlalsgur0215@gmail.com>
2026-04-12 21:59:48 +0900

    Create 2438.cpp

---
+++ C++/Baekjoon/Level_3/2438.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int num;
+    cin >> num;
+    for (int i = 0; i < num; i++){
+        for (int k = 0; k <= i; k++){
+            cout << "*";
+        }
+        cout << '\n';
+    }
+}
