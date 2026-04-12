58ef669a1947c72de334c272665ba2bc494768d3
miyuck <chlalsgur0215@gmail.com>
2026-04-12 16:48:14 +0900

    Create 9498.cpp

---
+++ C++/Baekjoon/Level_2/9498.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A;
+    cin >> A;
+    switch (A / 10){
+        case 10:
+        case 9:
+            cout << 'A';
+            break;
+        case 8:
+            cout << 'B';
+            break;
+        case 7:
+            cout << 'C';
+            break;
+        case 6:
+            cout << 'D';
+            break;
+        default:
+            cout << 'F';
+            break;
+    }
+}
