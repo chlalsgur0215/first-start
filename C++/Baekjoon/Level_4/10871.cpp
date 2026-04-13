59a376b00342ff7eca885206b17edfa37d5777c6
miyuck <chlalsgur0215@gmail.com>
2026-04-13 20:22:06 +0900

    Create 10871.cpp

---
+++ C++/Baekjoon/Level_4/10871.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N, X, A;
+    cin >> N >> X;
+    for(int i=0; i<N; i++){
+        cin >> A;
+        if(X > A){
+            cout << A << ' ';
+        }
+    }
+}
+//- ------------------------------------
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int N, X, A[10000];
+//     cin >> N >> X;
+//     for(int i=0; i<N; i++){
+//         cin >> A[i];
+//         if(A[i] < X){
+//             cout << A[i] << ' ';
+//         }
+//     }
+    
+// }
